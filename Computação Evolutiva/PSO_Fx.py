2705a28fe056d332c3f5752c912e670c35b0fffe
renholds <58077899+renholds@users.noreply.github.com>
2019-12-07 16:00:11 -0300

    Update PSO_Fx.py
    plotando o gráfico de evolução das partículas, e dos melhores indivíduos de cada conjunto de parâmetros

--- Computação Evolutiva/PSO_Fx.py
+++ Computação Evolutiva/PSO_Fx.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np 
+import matplotlib.pyplot as plt
 
 """     
     Usando alguma biblioteca ou implementando diretamente em alguma linguagem de programação, desenvolva uma solução para minimizar a função de Rosenbrock (em anexo). 
@@ -72,55 +73,44 @@ class Area():
             particula.velocidade = new_velocidade
             particula.move()
 
-def plot(resultados):
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots()
-    ax.plot(resultados[0][0][0],resultados[0][0][1],'r',label='params 1')
-    ax.plot(resultados[1][0][0],resultados[1][0][1],'b',label='params 2')
-    ax.plot(resultados[2][0][0],resultados[2][0][1],'g',label='params 3')
-   
-    ax.set(xlabel='Geracao', ylabel='Fitness (Media)', title='Convergência')
-    plt.legend(loc='lower right')
-    ax.grid()
+def plot_final(resultados):
+    
+    g1 = (resultados[0][0][0], resultados[0][0][1])
+    g2 = (resultados[1][0][0], resultados[1][0][1])
+    g3 = (resultados[2][0][0], resultados[2][0][1])
+    
+    data = (g1, g2, g3)
+    colors = ("red", "green", "blue")
+    groups = ("Conjunto de parametros 1", "Conjunto de parametros 2", "Conjunto de parametros 3")
+    
+    # Create plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, facecolor="1.0")
+    
+    for data, color, group in zip(data, colors, groups):
+        x, y = data
+        ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=50, label=group)
+    
+    plt.title('Melhores indivíduos de cada conjunto de parametrizações')
+    plt.legend(loc=2)
     plt.show()
-
-# def plot_grafico(lista_pso):   
-    # import matplotlib.pyplot as plt
-    # from mpl_toolkits.mplot3d import Axes3D
-
-    # lista_posicoes = []
-    # lista_parametros = []
-    # lista_interacoes = []
-    # x = []
-    # y = []
-    # for item in lista_pso:
-    #    lista_posicoes.append(item[0]) 
-    #    x.append(item[0][0])
-    #    y.append(item[0][1])
-    # #    lista_interacoes.append(item[1]) 
-    # #    lista_parametros.append(item[2])
-    # # plotting the points  
-    # # plt.plot(x, y)
-    # # plt.plot(x, y, label = str(item[2]) + "com " + str(item[1]))    
-    # # plt.title('Two lines on same graph!') 
-    # # plotting the points  
-    # # plt.plot(x, y, color='green', linestyle='dashed', linewidth = 3, 
-    # #         marker='o', markerfacecolor='blue', markersize=12)
-    # fig, ax  = plt.subplots()
-    # scatter  = ax.scatter(x, y, c=3)
-    # legenda  = ax.legend(*scatter.legend_elements(), loc="upper left", title="Classes")
-    # ax.add_artist(legenda)
-    # # show a legend on the plot 
-    # # plt.legend() 
-    # # plt.xlabel('x') 
-    # # plt.ylabel('y') 
-    # plt.show()
+    
+def plot(resultados):
+    # Create plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, facecolor="1.0")
+    for index, vetor in enumerate(resultados):
+        ax.scatter(vetor[0], vetor[1], alpha=0.8, c="gray", edgecolors='none', s=50)
+    plt.title('Evolução do enxame')
+    plt.legend(loc=2)
+    plt.show()
             
 def main():
     """ W, c1 e c2 """
     lista_parametros1 = [0.5, 0.8, 0.9] 
     lista_parametros2 = [0.7, 0.5, 0.7]
     lista_parametros3 = [1, 0.4, 0.5]
+    list_posicoes= []
     def PSO(parametros):
         n_interacoes = 50
         lista_min_max = [-2, 2]
@@ -130,7 +120,7 @@ def main():
         vetor_particulas = [Particula() for _ in range(procura_area.n_particulas)]
         procura_area.particulas = vetor_particulas
         procura_area.print_particulas()
-
+            
         interacao = 0
         while(interacao < n_interacoes):
             procura_area.set_pbest()    
@@ -140,7 +130,8 @@ def main():
                 print("A melhor posição é ", procura_area.gbest_posicao, " com ", interacao, " interações \nParâmetros: ", parametros[0], ', ', parametros[1], ', ', parametros[2])
                 procura_area.gbest_posicao
                 break
-
+            #guarda todas as posicoes)
+            list_posicoes.append(procura_area.gbest_posicao)
             procura_area.move_particulas(parametros[0], parametros[1], parametros[2])
             interacao += 1
         return procura_area.gbest_posicao, interacao, parametros
@@ -149,8 +140,8 @@ def main():
     lista_resultados.append(PSO(lista_parametros1))
     lista_resultados.append(PSO(lista_parametros2))
     lista_resultados.append(PSO(lista_parametros3))
-    # plot_grafico(lista_resultados)
-    plot(lista_resultados)
+    plot(list_posicoes)
+    plot_final(lista_resultados)
 
 if __name__ == '__main__':
     main()
